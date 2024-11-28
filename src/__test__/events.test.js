9fcd30a62ce42ce6f1698fe9f5334c6f0e5c1707
John Veresk <john.veresk@proton.me>
2024-11-28 14:46:45 +0200

    refactor check if event exists

--- src/__test__/events.test.js
+++ src/__test__/events.test.js
@@ -1,16 +1,16 @@
 const { createEventHandler } = require("../events");
-const { WSMock, DBMock, createDBMock } = require("../utils/mocks");
+const { WSMock, createDBMock } = require("../utils/mocks");
 const { silenceLogs } = require("../utils/test-utils");
 
 describe("events", () => {
   silenceLogs();
 
-  it("should send OK false in case db issues", async () => {
+  it("should send OK false with connection error in case of db issues", async () => {
     const event = { id: "event-id", content: "hello" };
     const ws = new WSMock();
     const db = createDBMock();
-    db.events.insertOne = jest.fn(() => Promise.reject(Error("e")));
-    const error = Error("e");
+    const error = "err";
+    db.events.insertOne = jest.fn(() => Promise.reject(error));
 
     await createEventHandler({ db })({ ws, event });
     expect(console.log).toHaveBeenCalledWith(
@@ -27,6 +27,22 @@ describe("events", () => {
       ])
     );
   });
+
+  it("should send OK true with duplicate message in case of the duplicate", async () => {
+    const event = { id: "event-id", content: "hello" };
+    const ws = new WSMock();
+    const db = createDBMock();
+    await db.events.insertOne(event);
+    const message = "duplicate: already have this event";
+    await createEventHandler({ db })({ ws, event });
+
+    expect(console.log).toHaveBeenCalledWith(message);
+    expect(ws.send).toHaveBeenCalledTimes(1);
+    expect(ws.send).toHaveBeenCalledWith(
+      JSON.stringify(["OK", "event-id", true, message])
+    );
+  });
+
   it("should send OK true if data was saved in the db", async () => {
     const event = { id: "event-id", content: "hello" };
     const ws = new WSMock();
@@ -34,12 +50,7 @@ describe("events", () => {
     await createEventHandler({ db })({ ws, event });
     expect(ws.send).toHaveBeenCalledTimes(1);
     expect(ws.send).toHaveBeenCalledWith(
-      JSON.stringify([
-        "OK",
-        "event-id",
-        true,
-        "",
-      ])
+      JSON.stringify(["OK", "event-id", true, ""])
     );
   });
 });
--- src/events.js
+++ src/events.js
@@ -4,12 +4,24 @@ const createEventHandler =
   ({ db }) =>
   async ({ ws, event }) => {
     try {
-      await db.events.insertOne(event);
-      sendOk({
-        ws,
-        eventId: event.id,
-        success: true,
-      });
+      const prev = await db.events.findOne(event.id);
+      if (!prev) {
+        await db.events.insertOne(event);
+        sendOk({
+          ws,
+          eventId: event.id,
+          success: true,
+        });
+      } else {
+        const message = "duplicate: already have this event";
+        console.log(message);
+        sendOk({
+          ws,
+          eventId: event.id,
+          success: true,
+          message,
+        });
+      }
     } catch (error) {
       const message = "error: could not connect to the database";
       console.log(message, error);
