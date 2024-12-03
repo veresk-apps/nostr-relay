78023048d7e1b75a7d771ea503d66a6c5a37c1ce
John Veresk <john.veresk@proton.me>
2024-12-03 16:56:57 +0200

    refactor req tests

--- src/handlers/__test__/req.test.js
+++ src/handlers/__test__/req.test.js
@@ -5,117 +5,76 @@ const { createReqHandler } = require("../req");
 describe("req", () => {
   silenceLogs();
 
-  it("should send CLOSE if no events", async () => {
-    const { subscription, ws, db, queries } = given();
+  describe("filters", () => {
+    it("should send an event for ids filter", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ ids: ["1", "2"] }],
+        events: [{ id: "1" }],
+      });
 
-    await createReqHandler({ db })({ ws, subscription, queries });
-    expect(ws.send).toHaveBeenCalledTimes(1);
-    expectClosedSent({
-      ws,
-      subscription,
-      message: "error: no filters specified",
-    });
-  });
+      await insertEvents({ db, events });
 
-  it("should send EOSE if no events", async () => {
-    const { subscription, ws, db, queries } = given({ queries: [{ ids: [] }] });
+      await createReqHandler({ db })({ ws, subscription, queries });
 
-    await createReqHandler({ db })({ ws, subscription, queries });
-    expect(ws.send).toHaveBeenCalledTimes(1);
-    expectEOSESent({ ws, subscription });
-  });
-
-  it("should send an event for ids filter", async () => {
-    const { subscription, ws, db, queries, events } = given({
-      queries: [{ ids: ["1", "2"] }],
-      events: [{ id: "1" }],
+      expect(ws.send).toHaveBeenCalledTimes(2);
+      expectEventsSent({ ws, subscription, events });
     });
 
-    await insertEvents({ db, events });
+    it("should send an event for authors filter", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ authors: ["pub1"] }],
+        events: [{ id: "1", pubkey: "pub1" }],
+      });
 
-    await createReqHandler({ db })({ ws, subscription, queries });
+      await insertEvents({ db, events });
+      await createReqHandler({ db })({ ws, subscription, queries });
 
-    expect(ws.send).toHaveBeenCalledTimes(2);
-    expectEventsSent({ ws, subscription, events });
-    expectEOSESent({ ws, subscription });
-  });
-
-  it("should send an event for authors filter", async () => {
-    const { subscription, ws, db, queries, events } = given({
-      queries: [{ authors: ["pub1"] }],
-      events: [{ id: "1", pubkey: "pub1" }],
+      expect(ws.send).toHaveBeenCalledTimes(2);
+      expectEventsSent({ ws, subscription, events });
     });
 
-    await insertEvents({ db, events });
-    await createReqHandler({ db })({ ws, subscription, queries });
+    it("should not send an event if authors filter do not match", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ authors: ["pubx"] }],
+        events: [{ id: "1", pubkey: "pub1" }],
+      });
 
-    expect(ws.send).toHaveBeenCalledTimes(2);
-    expectEventsSent({ ws, subscription, events });
-    expectEOSESent({ ws, subscription });
-  });
+      await insertEvents({ db, events });
 
-  it("should not send an event if authors filter do not match", async () => {
-    const { subscription, ws, db, queries, events } = given({
-      queries: [{ authors: ["pubx"] }],
-      events: [{ id: "1", pubkey: "pub1" }],
+      await createReqHandler({ db })({ ws, subscription, queries });
+
+      expect(ws.send).toHaveBeenCalledTimes(1);
     });
 
-    await insertEvents({ db, events });
+    it("should send events for ids filter", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ ids: ["1", "2"] }],
+        events: [{ id: "1" }, { id: "2" }],
+      });
 
-    await createReqHandler({ db })({ ws, subscription, queries });
+      await insertEvents({ db, events });
 
-    expect(ws.send).toHaveBeenCalledTimes(1);
-    expectEOSESent({ ws, subscription });
-  });
+      await createReqHandler({ db })({ ws, subscription, queries });
 
-  it("should send events for ids filter", async () => {
-    const { subscription, ws, db, queries, events } = given({
-      queries: [{ ids: ["1", "2"] }],
-      events: [{ id: "1" }, { id: "2" }],
+      expect(ws.send).toHaveBeenCalledTimes(3);
+      expectEventsSent({ ws, subscription, events });
     });
 
-    await insertEvents({ db, events });
+    it("should handle many filters", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ ids: ["1"] }, { authors: ["pub2"] }],
+        events: [
+          { id: "1", pubkey: "pub1" },
+          { id: "2", pubkey: "pub2" },
+        ],
+      });
 
-    await createReqHandler({ db })({ ws, subscription, queries });
+      await insertEvents({ db, events });
 
-    expect(ws.send).toHaveBeenCalledTimes(3);
-    expectEventsSent({ ws, subscription, events });
-    expectEOSESent({ ws, subscription });
-  });
+      await createReqHandler({ db })({ ws, subscription, queries });
 
-  it("should handle many filters", async () => {
-    const { subscription, ws, db, queries, events } = given({
-      queries: [{ ids: ["1"] }, { authors: ["pub2"] }],
-      events: [
-        { id: "1", pubkey: "pub1" },
-        { id: "2", pubkey: "pub2" },
-      ],
-    });
-
-    await insertEvents({ db, events });
-
-    await createReqHandler({ db })({ ws, subscription, queries });
-
-    expect(ws.send).toHaveBeenCalledTimes(3);
-    expectEventsSent({ ws, subscription, events });
-    expectEOSESent({ ws, subscription });
-  });
-
-  it("should send CLOSED event in case of db failure", async () => {
-    const { subscription, ws, db, queries, events } = given({
-      queries: [{ ids: ["1"] }],
-      events: [{ id: "1" }],
-    });
-    const error = new Error("db failed");
-    db.events.findMany = jest.fn(() => Promise.reject(error));
-
-    await createReqHandler({ db })({ ws, subscription, queries });
-
-    expect(ws.send).toHaveBeenCalledTimes(1);
-    expectClosedSent({
-      ws,
-      subscription,
-      message: "error: could not connect to the database",
+      expect(ws.send).toHaveBeenCalledTimes(3);
+      expectEventsSent({ ws, subscription, events });
     });
   });
 
@@ -141,7 +100,6 @@ describe("req", () => {
 
       expect(ws.send).toHaveBeenCalledTimes(4);
       expectEventsSentInOrder({ ws, subscription, events: eventsSorted });
-      expectEOSESent({ ws, subscription });
     });
 
     it("should order events by id if timestamp is the same", async () => {
@@ -165,7 +123,66 @@ describe("req", () => {
 
       expect(ws.send).toHaveBeenCalledTimes(4);
       expectEventsSentInOrder({ ws, subscription, events: eventsSorted });
+    });
+  });
+
+  describe("EOSE", () => {
+    it("should send EOSE if no events", async () => {
+      const { subscription, ws, db, queries } = given({
+        queries: [{ ids: [] }],
+      });
+
+      await createReqHandler({ db })({ ws, subscription, queries });
+      expect(ws.send).toHaveBeenCalledTimes(1);
       expectEOSESent({ ws, subscription });
+    });
+
+    it("should send EOSE after all events have been sent", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ ids: ["1"] }, { authors: ["pub2"] }],
+        events: [
+          { id: "1", pubkey: "pub1" },
+          { id: "2", pubkey: "pub2" },
+        ],
+      });
+
+      await insertEvents({ db, events });
+      await createReqHandler({ db })({ ws, subscription, queries });
+
+      expect(ws.send).toHaveBeenCalledTimes(3);
+      expectEOSESent({ ws, subscription });
+    });
+  });
+
+  describe("CLOSED", () => {
+    it("should send CLOSED if no events", async () => {
+      const { subscription, ws, db, queries } = given();
+
+      await createReqHandler({ db })({ ws, subscription, queries });
+      expect(ws.send).toHaveBeenCalledTimes(1);
+      expectClosedSent({
+        ws,
+        subscription,
+        message: "error: no filters specified",
+      });
+    });
+
+    it("should send CLOSED event in case of db failure", async () => {
+      const { subscription, ws, db, queries, events } = given({
+        queries: [{ ids: ["1"] }],
+        events: [{ id: "1" }],
+      });
+      const error = new Error("db failed");
+      db.events.findMany = jest.fn(() => Promise.reject(error));
+
+      await createReqHandler({ db })({ ws, subscription, queries });
+
+      expect(ws.send).toHaveBeenCalledTimes(1);
+      expectClosedSent({
+        ws,
+        subscription,
+        message: "error: could not connect to the database",
+      });
     });
   });
 });
